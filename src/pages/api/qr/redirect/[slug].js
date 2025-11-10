65d239e199d26586d93f988bf7680e5eea4d01af
Frank Lan <fwlan90@gmail.com>
2025-11-10 15:50:23 -0800

    Remove caching so redirect works properly.
    Respond 302 (Found) instead of 301 (Moved Permanently)
    If this was not done, then QR link continues to redirect to old URL.

--- src/pages/api/qr/redirect/[slug].js
+++ src/pages/api/qr/redirect/[slug].js
@@ -2,10 +2,13 @@ import prisma from "../../../../lib/prisma";
 
 export default async function handler(req, res) {
   const { slug } = req.query;
+
   const link = await prisma.qrLink.findUnique({ where: { slug } });
   if (!link || link.deletedAt) {
     return res.status(404).send("Not found");
   }
-  res.writeHead(301, { Location: link.destination });
+
+  res.setHeader("Cache-Control", "no-store");
+  res.writeHead(302, { Location: link.destination });
   res.end();
 }
